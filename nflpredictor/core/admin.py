aaf0f9da51d76baee7d53b301d38c4bbefbcc064
Samuel Kim <samuelkim@samuels-air.mynetworksettings.com>
2025-08-14 21:19:20 -0400

    remove the now ignored directory "nflpredictor"

--- nflpredictor/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Season, Team, Game, Play
-# Register your models here.
-
-admin.site.register(Season)
-admin.site.register(Team)
-admin.site.register(Game)
-admin.site.register(Play)
